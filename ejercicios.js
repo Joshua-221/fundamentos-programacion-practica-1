53e29dc382615f529b83aca4d12428393d2e0500
Joshua-221 <joshua.cruz@tecdesoftware.edu.mx>
2025-11-11 20:17:16 -0600

    Ejercicio 20 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -311,8 +311,7 @@ function sumaMatriz(matriz) {
  * Ejemplo: obtenerFila([[1, 2], [3, 4], [5, 6]], 1) debe retornar [3, 4]
  */
 function obtenerFila(matriz, indiceFila) {
-  // TODO: Retorna la fila indicada
-  return [];
+  return matriz[indiceFila]; //función simple para agregar una fila concuerdo a los números colocados 
 }
 
 /**
